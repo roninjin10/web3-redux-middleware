af2a9ff5afafab34a05c765c642647ca60f25e2e
William Cory <willcory10@gmail.com>
2018-06-10 09:22:08 -0700

    add errors to PromiEvent if no Promise ejected

--- test/PromiEvent.js
+++ test/PromiEvent.js
@@ -1,8 +1,14 @@
-import Promise from 'bluebird'
 import EventEmitter from 'events'
+import Promise from 'bluebird'
 
 export default class PromiEvent extends EventEmitter {
   constructor(promise) {
+    if (!promise) {
+      throw new Error('no promise provided');
+    }
+    if (typeof promise.then !== 'function') {
+      throw new Error('no promise provided');
+    }
     this.then = promise.then.bind(promise)
     this.catch = promise.catch.bind(promise)
   }
