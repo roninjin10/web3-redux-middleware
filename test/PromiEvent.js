cc8c2a4eeca055b9fad258eae0e6ac0f57a7d43a
William Cory <willcory10@gmail.com>
2018-06-10 09:06:59 -0700

    refactor PromiEvent from pseudoclassical to es6 Class

--- test/PromiEvent.js
+++ test/PromiEvent.js
@@ -1,15 +1,9 @@
 import Promise from 'bluebird'
 import EventEmitter from 'events'
-import Promise from 'bluebird'
-import EventEmitter from 'events'
 
-const PromiEvent = function(promise) {
-  EventEmitter.call(this);
-  this.then = promise.then.bind(promise)
-  this.catch = promise.catch.bind(promise)
-};
-
-PromiEvent.prototype = Object.create(EventEmitter.prototype);
-PromiEvent.constructor = EventEmitter;
-
-export default PromiEvent
+export default class PromiEvent extends EventEmitter {
+  constructor(promise) {
+    this.then = promise.then.bind(promise)
+    this.catch = promise.catch.bind(promise)
+  }
+}
