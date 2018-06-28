806950b599edb63f84c0e998fe1e105749a2ce90
William Cory <willcory10@gmail.com>
2018-06-27 22:04:19 -0700

    delete unnecessary constants

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,9 @@
-export const PENDING = 'PENDING'
-export const FULFILLED = 'FULFILLED'
-export const REJECTED = 'REJECTED'
-export const HASHED = 'HASHED'
-export const CONFIRMED = 'CONFIRMED'
-export const RECIEPT = 'RECIEPT'
-export const ERROR = 'ERROR'
 export const defaultTypes = {
-  pending: PENDING,
-  fulfilled: FULFILLED,
-  rejected: REJECTED,
-  transactionHash: HASHED,
-  confirmation: CONFIRMED,
-  reciept: RECIEPT,
-  error: ERROR,
+  pending: 'PENDING',
+  fulfilled: 'FULFILLED',
+  rejected: 'REJECTED',
+  transactionHash: 'HASHED',
+  confirmation: 'CONFIRMED',
+  reciept: 'RECIEPT',
+  error: 'ERROR',
 }
